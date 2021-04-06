f7140975bbece43986ffa6c33a48240c75235fc6
Laura <65158950+lauratro@users.noreply.github.com>
2021-04-06 17:31:48 +0200

    js mobile menu link delay

--- mobile.js
+++ mobile.js
@@ -2,9 +2,17 @@
 let scrollDownMenu = () => {
   let hamburger = document.querySelector(".hamburger");
   let mobileMenu = document.querySelector(".mobile-menu");
+  let links = document.querySelectorAll(".menu-list-mobile li");
+  let icons = document.querySelectorAll(".menu-list-mobile i");
 
   hamburger.addEventListener("click", () => {
     mobileMenu.classList.toggle("open");
+    links.forEach((x) => {
+      x.classList.toggle("end");
+    });
+    icons.forEach((y) => {
+      y.classList.toggle("iconEnd");
+    });
   });
 };
 scrollDownMenu();
