858ca02cc81763a893019c4e3922ba8f9c5cd1fe
Laura <65158950+lauratro@users.noreply.github.com>
2021-04-12 09:59:03 +0200

    Added url api to the form

--- script.js
+++ script.js
@@ -18,10 +18,17 @@ function writeText() {
 
 s = setInterval("writeText()", 100);
 
-//Registration Form
+//Registration Form with url Api
 function results() {
+  let params = new URL(document.location).searchParams;
+  let name = params.get("name");
+  document.getElementById("data").innerHTML = name;
+}
+results();
+// LocalStorage method
+/*function results() {
   let name = document.getElementById("name").value;
   localStorage.setItem("name1", name);
 }
 
-document.getElementById("data").innerHTML = localStorage.getItem("name1");
+document.getElementById("data").innerHTML = localStorage.getItem("name1");*/
